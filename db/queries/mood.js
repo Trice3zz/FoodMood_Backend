1f4fdab4442d6e328b474807bd47ad90c02f8ac7
Danielle <dani.camp1125@gmail.com>
2025-07-09 21:28:45 -0400

    testing code and adding components to see if it will affect changes to passing tests--Dani

--- db/queries/mood.js
+++ db/queries/mood.js
@@ -1,6 +1,16 @@
 import client from '../client.js'
 
-export async function createMoods(){
+
+
+export async function getMoods(){
     const result = await client.query('SELECT * FROM moods;')
     return result.rows;
 };
+
+export async function createMoods(emotion, description) {
+    const result = await client.query(
+      'INSERT INTO moods (emotion, description) VALUES ($1, $2) RETURNING *;',
+      [emotion, description]
+    );
+    return result.rows[0];
+  }
